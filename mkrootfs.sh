db6ca9594fb8a5cd62a3211ff4ed5e19b849ca21
Han Gao <rabenda.cn@gmail.com>
2025-12-26 00:22:59 +0800

    software: update software list
    Signed-off-by: Han Gao <rabenda.cn@gmail.com>

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -26,15 +26,15 @@ else
 fi
 
 # == packages ==
-BASE_TOOLS="binutils file tree sudo bash-completion u-boot-menu initramfs-tools openssh-server network-manager dnsmasq-base libpam-systemd ppp wireless-regdb wpasupplicant libengine-pkcs11-openssl iptables systemd-timesyncd vim usbutils libgles2 parted"
+BASE_TOOLS="binutils file tree sudo bash-completion network-manager dnsmasq-base libpam-systemd ppp wireless-regdb wpasupplicant libengine-pkcs11-openssl iptables usbutils libgles2 parted"
 #XFCE_DESKTOP="xorg xfce4 desktop-base lightdm xfce4-terminal tango-icon-theme xfce4-notifyd xfce4-power-manager network-manager-gnome xfce4-goodies pulseaudio alsa-utils dbus-user-session rtkit pavucontrol thunar-volman eject gvfs gvfs-backends udisks2 dosfstools e2fsprogs e2fsprogs libblockdev-crypto3 ntfs-3g polkitd exfat-fuse "
 #GNOME_DESKTOP="gnome-core avahi-daemon desktop-base file-roller gnome-tweaks gstreamer1.0-libav gstreamer1.0-plugins-ugly libgsf-bin libproxy1-plugin-networkmanager network-manager-gnome"
-KDE_DESKTOP="task-kde-desktop"
+KDE_DESKTOP="task-kde-desktop task-ssh-server task-english"
 BENCHMARK_TOOLS="glmark2 mesa-utils vulkan-tools iperf3 stress-ng"
 #FONTS="fonts-crosextra-caladea fonts-crosextra-carlito fonts-dejavu fonts-liberation fonts-liberation2 fonts-linuxlibertine fonts-noto-core fonts-noto-cjk fonts-noto-extra fonts-noto-mono fonts-noto-ui-core fonts-sil-gentium-basic"
 FONTS="fonts-noto-core fonts-noto-cjk fonts-noto-mono fonts-noto-ui-core"
 INCLUDE_APPS="firefox vlc gimp chromium"
-EXTRA_TOOLS="i2c-tools net-tools ethtool wget python3-ruyi cloud-init cloud-guest-utils"
+EXTRA_TOOLS="i2c-tools net-tools ethtool wget python3-ruyi cloud-init cloud-guest-utils u-boot-menu initramfs-tools"
 LIBREOFFICE="libreoffice-base \
 libreoffice-calc \
 libreoffice-core \
@@ -53,7 +53,7 @@ libreoffice-sdbc-postgresql \
 libreoffice-wiki-publisher \
 "
 DOCKER="docker.io apparmor ca-certificates cgroupfs-mount git needrestart xz-utils"
-ADDONS="initramfs-tools firmware-amd-graphics firmware-nvidia-graphics firmware-intel-graphics firmware-intel-misc firmware-realtek"
+ADDONS="systemd-timesyncd vim firmware-amd-graphics firmware-nvidia-graphics firmware-intel-graphics firmware-intel-misc firmware-realtek"
 
 machine_info() {
     uname -a
@@ -72,7 +72,7 @@ init() {
     apt update
 
     # create flash image
-    fallocate -l 9G $ROOT_IMG
+    fallocate -l 12G $ROOT_IMG
 }
 
 install_deps() {
@@ -115,8 +115,7 @@ img_setup() {
 make_rootfs() {
     mmdebstrap --architectures=riscv64 \
     --skip=check/empty \
-    --include="ca-certificates debian-ports-archive-keyring locales locales-all dosfstools \
-        $BASE_TOOLS $KDE_DESKTOP $BENCHMARK_TOOLS $FONTS $INCLUDE_APPS $EXTRA_TOOLS $LIBREOFFICE $ADDONS" \
+    --include="ca-certificates debian-archive-keyring revyos-keyring locales locales-all dosfstools" \
     trixie "$CHROOT_TARGET" \
     "deb https://mirror.iscas.ac.cn/revyos/revyos-kernels/ revyos-kernels main" \
     "deb https://mirror.iscas.ac.cn/revyos/trixie/revyos-addons/ trixie main" \
@@ -138,13 +137,6 @@ EOF
         cp -v revyos-release rootfs/etc/revyos-release
     fi
 
-    # Install revyos-kerings
-    sudo chroot $CHROOT_TARGET /bin/bash << EOF
-export DEBIAN_FRONTEND=noninteractive
-wget https://mirror.iscas.ac.cn/revyos/trixie/revyos-addons/pool/main/r/revyos-keyring/revyos-keyring_2025.03.28_all.deb
-dpkg -i revyos-keyring_2025.03.28_all.deb
-EOF
-
     # clean up source.list
     cat > $CHROOT_TARGET/etc/apt/sources.list << EOF
 deb https://mirror.iscas.ac.cn/revyos/revyos-kernels/ revyos-kernels main
@@ -152,6 +144,12 @@ deb https://mirror.iscas.ac.cn/revyos/trixie/revyos-addons/ trixie main
 deb https://mirror.iscas.ac.cn/revyos/trixie/revyos-base/ trixie main contrib non-free non-free-firmware
 EOF
 
+    sudo chroot $CHROOT_TARGET /bin/bash << EOF
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install -y $KDE_DESKTOP $FONTS $EXTRA_TOOLS $ADDONS $BENCHMARK_TOOLS $INCLUDE_APPS
+#"$BASE_TOOLS $KDE_DESKTOP $BENCHMARK_TOOLS $FONTS $INCLUDE_APPS $EXTRA_TOOLS $ADDONS"
+EOF
     # Add update-u-boot config
 if [ "$MODEL" = "pioneer" ]; then
     cat > $CHROOT_TARGET/etc/default/u-boot << EOF
@@ -176,6 +174,7 @@ fi
 
     # Install kernel
     sudo chroot $CHROOT_TARGET /bin/bash << EOF
+export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y $KERNEL
 u-boot-update
