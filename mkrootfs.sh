54f591864e9e98fdfcf9e190896c501a0645b6a8
Han Gao <gaohan@iscas.ac.cn>
2026-05-04 19:13:41 +0800

    chore: update kernel to debian bpo kernel
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -9,8 +9,8 @@ TIMESTAMP=$(date +%Y%m%d-%H%M%S)
 ROOT_IMG=revyos-${MODEL}-${TIMESTAMP}.img
 
 # == kernel variables ==
-KERNEL_pioneer="linux-headers-6.18-revyos linux-image-6.18-revyos"
-KERNEL_sg2044="linux-headers-6.18-revyos linux-image-6.18-revyos"
+KERNEL_pioneer="linux-headers-riscv64 linux-image-riscv64"
+KERNEL_sg2044="linux-headers-riscv64 linux-image-riscv64"
 KERNEL_pisces="linux-headers-6.6-pisces linux-image-6.6-pisces"
 KERNEL=$(eval echo '$'"KERNEL_${MODEL}")
 
